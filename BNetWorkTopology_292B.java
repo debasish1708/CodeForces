627b3e86488c3cf4605c16f8ed094ee0f52a8b2c
debasish1708 <debasishdas1708@gmail.com>
2026-04-19 13:38:29 +0530

    B - Network Topology 292B

--- BNetWorkTopology_292B.java
+++ BNetWorkTopology_292B.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BNetWorkTopology {
+public class BNetWorkTopology_292B {
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
